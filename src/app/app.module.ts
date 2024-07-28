2dd117203932f2e9c184888b1f713866a2180073
Bhavik Choudhary <bhavikchoudhary146@gmail.com>
2024-07-29 00:55:56 +0530

    adding paginator feature, get pageSize from paginator, api changes, etc

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http'
 
 import { MatInputModule } from '@angular/material/input';
@@ -11,20 +11,23 @@ import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatPaginatorModule} from '@angular/material/paginator';
 
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { HeaderComponent } from './header/header.component';
 import {PostListComponent} from './posts/post-list/post-list.component'
+import { LoginComponent } from './auth/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent, PostCreateComponent, PostListComponent, HeaderComponent,
+    AppComponent, PostCreateComponent, PostListComponent, HeaderComponent, LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
+    FormsModule,
     HttpClientModule,
     NoopAnimationsModule,
     MatInputModule,
@@ -32,7 +35,8 @@ import {PostListComponent} from './posts/post-list/post-list.component'
     MatCardModule,
     MatButtonModule,
     MatToolbarModule,
-    MatProgressSpinnerModule
+    MatProgressSpinnerModule,
+    MatPaginatorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
