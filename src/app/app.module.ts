25deb95b64811210ff5796eb52b130c1c2d44f0c
Bhavik Choudhary <bhavikchoudhary146@gmail.com>
2024-08-23 01:20:16 +0530

    seperate materialModule added,, seperate components by adding postModule/authModule,, lazy loading auth module implementation,, added auth-routing module,, navigation changes,, minor bug fixes resolved

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,48 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http'
-
-import { MatInputModule } from '@angular/material/input';
-import {MatExpansionModule} from '@angular/material/expansion';
-import {MatCardModule} from '@angular/material/card';
-import {MatButtonModule} from '@angular/material/button';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
-import {MatPaginatorModule} from '@angular/material/paginator';
-import {MatDialogModule} from '@angular/material/dialog';
 
 
 import { AppComponent } from './app.component';
-import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { HeaderComponent } from './header/header.component';
-import {PostListComponent} from './posts/post-list/post-list.component'
-import { LoginComponent } from './auth/login/login.component';
-import { SignupComponent } from './auth/signup/signup.component';
 import { AuthInterceptor } from './auth/auth-interceptor';
 import { ErrorInterceptor } from './error-interceptor';
 import { ErrorComponent } from './error/error.component';
 
+import { AngularMaterialModule } from './angular-material.module'
+import { PostModule } from './posts/posts.module';
+
 @NgModule({
   declarations: [
-    AppComponent, PostCreateComponent, PostListComponent, HeaderComponent, LoginComponent, SignupComponent, ErrorComponent
+    AppComponent, HeaderComponent, ErrorComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule,
-    FormsModule,
-    HttpClientModule,
     NoopAnimationsModule,
-    MatInputModule,
-    MatExpansionModule,
-    MatCardModule,
-    MatButtonModule,
-    MatToolbarModule,
-    MatProgressSpinnerModule,
-    MatPaginatorModule,
-    MatDialogModule
+    HttpClientModule,
+    AngularMaterialModule,
+    PostModule,
   ],
   providers: [
     {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi:true },
